21a6ebe58185f90859a4974f177c9cdd4d805363
unknown <sherrycat@yeah.net>
2017-10-02 22:56:47 +0800

    save

---
+++ src/Search.java
@@ -0,0 +1,14 @@
+public class Search {
+    String command;
+    InvertedIndex invertedIndex;
+
+    public Search(String command, InvertedIndex invertedIndex) {
+        this.command = command;
+        this.invertedIndex = invertedIndex;
+    }
+
+    public void resolveCom(){
+
+    }
+}
+
